4e1f7917df0d49577f270b2a837d0aefc2a3c274
Lordee <lordee.decastro@yahoo.com>
2019-04-10 15:20:35 +1200

    Button is present

--- PROJ0 Flicker Fusion Threshold/src/main.cpp
+++ PROJ0 Flicker Fusion Threshold/src/main.cpp
@@ -1,39 +1,52 @@
 #include <Arduino.h>
 
 uint32_t ledlight = 0;
-int ledState = LOW;
+uint32_t button = 12;
+uint32_t ledState = LOW;
 unsigned long previousMillis = 0;
 float interval;
 float value = 0;
 float frequency = 0;
+const uint32_t DEBOUNCETIME_MS = 100;
+void ISR();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledlight, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button), ISR, FALLING);
   Serial.begin(9600);
-  
+
 }
 
 void loop() {
    unsigned long currentMillis = millis();
-   value = analogRead(A0);
-   frequency = (map(value, 0 , 1024, 0 , 100));
+   value = (float)analogRead(A0);
+   frequency = (map(value, 0 , 1024, 0 , 60));
    interval = (1/frequency) * 1000;
 
   if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
     previousMillis = currentMillis;
 
-    // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
 
-    // set the LED with the ledState of the variable:
     digitalWrite(ledlight, ledState);
-    Serial.print(analogRead(A0));
-    Serial.print(", ");
+   
+  }
+}
+
+void ISR() {
+  static uint32_t previousTime_ms = 0;
+  uint32_t currentTime_ms = millis();
+
+  if((currentTime_ms - previousTime_ms) > DEBOUNCETIME_MS);{
+     previousTime_ms = currentTime_ms;
+  Serial.print(frequency);
+    Serial.println("Hz ");
+    Serial.flush();
   }
 }
